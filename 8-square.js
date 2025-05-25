af7e6cc57dda6f1f8bd82aca32aefd72ad8e7629
Anele <anelenkomolity24@gmail.com>
2025-05-25 22:57:53 +0200

    Created a square using a loop

---
+++ 8-square.js
@@ -0,0 +1,15 @@
+    let args = process.argv[2];
+let x = Number(args);
+
+if (args === undefined || isNaN(x)) {
+    console.log("Missing number of occurences");
+} 
+ else {
+    for (let i = 0; i < x; i++) {
+        let row = "";
+        for (let j = 0; j < x; j++) {
+            row += "X";
+        }
+        console.log(row);
+    }
+}
